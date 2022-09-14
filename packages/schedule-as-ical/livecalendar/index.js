bff36f4e69d1768137d046343fbab0297f6e9086
Sebastian Dittrich <sebastian.dittrich@live.de>
2022-09-14 14:03:44 +0000

    WIP

--- packages/schedule-as-ical/livecalendar/index.js
+++ packages/schedule-as-ical/livecalendar/index.js
@@ -8,25 +8,22 @@ const node_fetch_1 = require("node-fetch");
 const https_1 = require("https");
 const promises_1 = require("stream/promises");
 const fs_1 = require("fs");
-function main() {
-    return { body: process.env.SHARE_URL };
+async function main() {
+    const get = (url) => new Promise((res) => (0, https_1.get)(url, res));
+    const cookieRes = await get(process.env.SHARE_URL);
+    const cookies = cookieRes.headers['set-cookie'].map(cookie => cookie.split(';')[0]).join('; ');
+    const res = await (0, node_fetch_1.default)("https://leibnizfh-my.sharepoint.com/personal/la_leibniz-fh_de/_api/files('Leibniz-FH/Stundenpl%C3%A4ne/IT-Security/dIT%202020-23/Stundenplanung_5Sem_dIT20.xlsx')/$value", {
+        headers: {
+            "Cookie": cookies
+        }
+    });
+    if (!res.ok)
+        throw new Error(`unexpected response ${res.statusText}`);
+    await (0, promises_1.pipeline)(res.body, (0, fs_1.createWriteStream)('./__downloaded_plan.xlsx'));
+    const plan = (0, excel_to_json_1.excelToJson)((0, xlsx_1.readFile)('./__downloaded_plan.xlsx'));
+    const ical = (0, json_to_ical_1.planToIcal)(plan);
+    return { body: ical };
 }
 exports.main = main;
 function test() {
-    const get = (url) => new Promise((res) => (0, https_1.get)(url, res));
-    (async () => {
-        const cookieRes = await get(process.env.SHARE_URL);
-        const cookies = cookieRes.headers['set-cookie'].map(cookie => cookie.split(';')[0]).join('; ');
-        const res = await (0, node_fetch_1.default)("https://leibnizfh-my.sharepoint.com/personal/la_leibniz-fh_de/_api/files('Leibniz-FH/Stundenpl%C3%A4ne/IT-Security/dIT%202020-23/Stundenplanung_5Sem_dIT20.xlsx')/$value", {
-            headers: {
-                "Cookie": cookies
-            }
-        });
-        if (!res.ok)
-            throw new Error(`unexpected response ${res.statusText}`);
-        await (0, promises_1.pipeline)(res.body, (0, fs_1.createWriteStream)('./__downloaded_plan.xlsx'));
-        const plan = (0, excel_to_json_1.excelToJson)((0, xlsx_1.readFile)('./__downloaded_plan.xlsx'));
-        const ical = (0, json_to_ical_1.planToIcal)(plan);
-        console.log(ical);
-    })();
 }
--- packages/schedule-as-ical/livecalendar/index.ts
+++ packages/schedule-as-ical/livecalendar/index.ts
@@ -7,32 +7,30 @@ import { pipeline } from 'stream/promises';
 import { createWriteStream } from 'fs';
 import { IncomingMessage } from 'http';
 
-export function main() {
-    return { body: process.env.SHARE_URL }
+export async function main() {
+    const get = (url: string) => new Promise<IncomingMessage>((res) => _get(url, res));
+
+    const cookieRes = await get(process.env.SHARE_URL as string)
+
+    const cookies = (cookieRes.headers['set-cookie'] as string[]).map(cookie => cookie.split(';')[0]).join('; ')
+
+    const res = await fetch("https://leibnizfh-my.sharepoint.com/personal/la_leibniz-fh_de/_api/files('Leibniz-FH/Stundenpl%C3%A4ne/IT-Security/dIT%202020-23/Stundenplanung_5Sem_dIT20.xlsx')/$value", {
+        headers: {
+            "Cookie": cookies
+        }
+    })
+
+    if (!res.ok) throw new Error(`unexpected response ${res.statusText}`);
+
+    await pipeline(res.body, createWriteStream('./__downloaded_plan.xlsx'));
+
+    const plan = excelToJson(readFile('./__downloaded_plan.xlsx'))
+    const ical = planToIcal(plan)
+
+    return { body: ical }
 }
 
 function test() {
 
-    const get = (url: string) => new Promise<IncomingMessage>((res) => _get(url, res));
 
-    (async () => {
-        const cookieRes = await get(process.env.SHARE_URL)
-
-        const cookies = cookieRes.headers['set-cookie'].map(cookie => cookie.split(';')[0]).join('; ')
-
-        const res = await fetch("https://leibnizfh-my.sharepoint.com/personal/la_leibniz-fh_de/_api/files('Leibniz-FH/Stundenpl%C3%A4ne/IT-Security/dIT%202020-23/Stundenplanung_5Sem_dIT20.xlsx')/$value", {
-            headers: {
-                "Cookie": cookies
-            }
-        })
-
-        if (!res.ok) throw new Error(`unexpected response ${res.statusText}`);
-
-        await pipeline(res.body, createWriteStream('./__downloaded_plan.xlsx'));
-
-        const plan = excelToJson(readFile('./__downloaded_plan.xlsx'))
-        const ical = planToIcal(plan)
-
-        console.log(ical)
-    })()
 }
